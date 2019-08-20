2414a13898761b61d844c479ce8aa1e619ecc137
E <erika.reiman@gmail.com>
2019-08-20 08:38:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+$(function(){
+
+
+  
+
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        var newItem = $(this).children('input').val();
+        var newListItem = '<li><span class="shopping-item">' + newItem + 
+            '</span>'+
+            '<div class="shopping-item-controls">' + 
+            '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' + 
+            '</button>' +
+          '  ' + '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' + '</li>'
+        $(this).parents()
+        .children('ul')
+        .children('li')
+        .last()
+        .after(newListItem);
+        $('#shopping-list-entry').val('');
+    });
+
+    /*$(:submit).on('click', function(){
+        event.preventDefault();
+        $(this).css('background-color', 'green');
+    });*/
+
+    $('.shopping-list').on('click', 'li div .shopping-item-toggle', function(){
+        $(this).parents()
+            .children('span')
+            .toggleClass('shopping-item__checked');
+    });
+
+    $('.shopping-list').on('click','li div .shopping-item-delete', function(){
+        $(this).parents('li')
+            .remove();
+    });
+
+    $('#shopping-list-entry').keypress(function(event){
+        var newItem = $(this).val();
+        var keyCode = event.which;
+        if (keyCode == 13) {
+            var newListItem = '<li><span class="shopping-item">' + newItem + 
+            '</span>'+
+            '<div class="shopping-item-controls">' + 
+            '<button class="shopping-item-toggle">' +
+            '<span class="button-label">check</span>' + 
+            '</button>' +  '  ' +
+          '<button class="shopping-item-delete">' +
+            '<span class="button-label">delete</span>' +
+          '</button>' + '</li>'
+             event.preventDefault();
+        
+            $(this).parents()
+            .children('ul')
+            .children('li')
+            .last()
+            .after(newListItem);
+            $('#shopping-list-entry').val('');
+        }
+       
+    });
+
+   
+
+    /*$('#shopping-list-entry').keypress(function(event){
+        var newItem = $(this).val();
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        if(keycode == '13'){
+            var newListItem = '<li>' + newItem + '</li>';
+            event.preventDefault();
+            $(this).parents()
+            .children('ul')
+             .append(newListItem);
+             $('#shopping-list-entry').val('');
+    });*/
+
+});
